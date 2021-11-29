811a86603df51c3b4df9391a6b191654a5300795
Vivek Buzruk <vivek_buzruk@yahoo.com>
2021-11-29 10:35:54 +0530

    complete 1'st release without generated files

--- jkube_console/build_deploy_run.sh
+++
@@ -1,3 +0,0 @@
-mvn clean install k8s:build k8s:resource k8s:apply
-mvn exec:java -Dexec.mainClass="coin.stech.jkube.helloConsole.App" 
-# mvn exec:java -Dexec.mainClass="io.kubernetes.client.examples.ExecExample" -Dexec.args="pod namespace sh" < test.sh
